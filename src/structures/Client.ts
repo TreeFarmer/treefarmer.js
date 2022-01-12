0087c3200906435b1aa60c6d9efa161da2ad5e02
Jaren Goldberg <fyrlex@gmail.com>
2022-01-12 06:22:28 -0500

    eslint and tsconfig reconfigured

--- src/structures/Client.ts
+++ src/structures/Client.ts
@@ -3,110 +3,110 @@ import req, { HTTPMethod } from 'petitio';
 import { ClientOptions, CutterData, CutterResponse, FarmResponse, PlotResponse, TreeResponse, StorageResponse, VehicleResponse, UserResponse, FarmData, PlotData, FarmStorageData, TreeData, UserData, VehicleData } from '../typings/index.js';
 
 export class Client {
-  private readonly _fullResponse: boolean;
+	private readonly _fullResponse: boolean;
 
-  /**
-   * Create a new API client
-   * @param {string} _token Your API Token
-   * @param {ClientOptions} options Options for the client
-   */
-  public constructor(private _token: string, options?: ClientOptions) {
-    this._fullResponse = options?.fullResponse ?? false;
-  }
+	/**
+	 * Create a new API client
+	 * @param {string} token Your API Token
+	 * @param {ClientOptions} options Options for the client
+	 */
+	public constructor(private token: string, options?: ClientOptions) {
+		this._fullResponse = options?.fullResponse ?? false;
+	}
 
-  /**
-   * Get a cutter or all cutters
-   * @param {string | number | undefined} search 
-   * @returns {Promise<CutterResponse | CutterData | Error>}
-   */
-  public async getCutter(search?: string | number): Promise<CutterResponse | CutterData | Error> {
-    const res: CutterResponse = await this.request('GET', '/cutters', null, { search: search ?? null });
+	/**
+	 * Get a cutter or all cutters
+	 * @param {string | number | undefined} search 
+	 * @returns {Promise<CutterResponse | CutterData | Error>}
+	 */
+	public async getCutter(search?: string | number): Promise<CutterResponse | CutterData | Error> {
+		const res: CutterResponse = await this.request('GET', '/cutters', null, { search: search ?? null });
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
-  }
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
+	}
 
-  /**
-   * Get a plot or all plots
-   * @param {string | number | undefined} search
-   * @returns {Promise<PlotResponse | PlotData | Error>}
-   */
-  public async getPlot(search?: string | number): Promise<PlotResponse | PlotData | Error> {
-    const res: PlotResponse = await this.request('GET', '/plots', null, { search: search ?? null });
+	/**
+	 * Get a plot or all plots
+	 * @param {string | number | undefined} search
+	 * @returns {Promise<PlotResponse | PlotData | Error>}
+	 */
+	public async getPlot(search?: string | number): Promise<PlotResponse | PlotData | Error> {
+		const res: PlotResponse = await this.request('GET', '/plots', null, { search: search ?? null });
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
-  }
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
+	}
 
-  /**
-   * Get a tree or all trees
-   * @param {string | number | undefined} search
-   * @returns {Promise<TreeResponse | TreeData | Error>}
-   */
-  public async getTree(search?: string | number): Promise<TreeResponse | TreeData | Error> {
-    const res: TreeResponse = await this.request('GET', '/trees', null, { search: search ?? null });
+	/**
+	 * Get a tree or all trees
+	 * @param {string | number | undefined} search
+	 * @returns {Promise<TreeResponse | TreeData | Error>}
+	 */
+	public async getTree(search?: string | number): Promise<TreeResponse | TreeData | Error> {
+		const res: TreeResponse = await this.request('GET', '/trees', null, { search: search ?? null });
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
-  }
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
+	}
 
-  /**
-   * Get a storage or all storages
-   * @param {string | number | undefined} search
-   * @returns {Promise<StorageResponse | FarmStorageData | Error>}
-   */
-  public async getStorage(search?: string | number): Promise<StorageResponse | FarmStorageData | Error> {
-    const res: StorageResponse = await this.request('GET', '/storages', null, { search: search ?? null });
+	/**
+	 * Get a storage or all storages
+	 * @param {string | number | undefined} search
+	 * @returns {Promise<StorageResponse | FarmStorageData | Error>}
+	 */
+	public async getStorage(search?: string | number): Promise<StorageResponse | FarmStorageData | Error> {
+		const res: StorageResponse = await this.request('GET', '/storages', null, { search: search ?? null });
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
-  }
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
+	}
 
-  /**
-   * Get a vehicle or all vehicles
-   * @param {string | number | undefined} search
-   * @returns {Promise<VehicleResponse | VehicleData | Error>}
-   */
-  public async getVehicle(search?: string | number): Promise<VehicleResponse | VehicleData | Error> {
-    const res: VehicleResponse = await this.request('GET', '/vehicles', null, { search: search ?? null });
+	/**
+	 * Get a vehicle or all vehicles
+	 * @param {string | number | undefined} search
+	 * @returns {Promise<VehicleResponse | VehicleData | Error>}
+	 */
+	public async getVehicle(search?: string | number): Promise<VehicleResponse | VehicleData | Error> {
+		const res: VehicleResponse = await this.request('GET', '/vehicles', null, { search: search ?? null });
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
-  }
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
+	}
 
-  /**
-   * Get a user's farm
-   * @param {Snowflake} id
-   * @returns {Promise<FarmResponse | FarmData | Error>}
-   */
-  public async getFarm(id: Snowflake): Promise<FarmResponse | FarmData | Error> {
-    const res: FarmResponse = await this.request('GET', `/users/${id}/farm`);
+	/**
+	 * Get a user's farm
+	 * @param {Snowflake} id
+	 * @returns {Promise<FarmResponse | FarmData | Error>}
+	 */
+	public async getFarm(id: Snowflake): Promise<FarmResponse | FarmData | Error> {
+		const res: FarmResponse = await this.request('GET', `/users/${id}/farm`);
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
-  }
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
+	}
 
-  /**
-   * Get a user's data
-   * @param {Snowflake} id
-   * @returns {Promise<UserResponse | UserData | Error>}
-   */
-  public async getUser(id: Snowflake): Promise<UserResponse | UserData | Error> {
-    const res: UserResponse = await this.request('GET', `/users/${id}`);
+	/**
+	 * Get a user's data
+	 * @param {Snowflake} id
+	 * @returns {Promise<UserResponse | UserData | Error>}
+	 */
+	public async getUser(id: Snowflake): Promise<UserResponse | UserData | Error> {
+		const res: UserResponse = await this.request('GET', `/users/${id}`);
 
-    if (res.error) throw new Error(res.message);
-    else return this._fullResponse ? res : res.data;
+		if (res.error) throw new Error(res.message);
+		else return this._fullResponse ? res : res.data;
 
-  }
+	}
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  private async request(method: HTTPMethod, path: string, params: any = {}, query: any = {}): Promise<any> {
-    const init = req('https://api.treefarmer.xyz', method).path(path).header('Authorization', this._token).body(params);
-    for (const [k, v] of Object.entries(query)) init.query(k, v);
-    const res = await init.send().catch(e => {
-      throw new Error(e);
-    });
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	private async request(method: HTTPMethod, path: string, params: any = {}, query: any = {}): Promise<any> {
+		const init = req('https://api.treefarmer.xyz', method).path(path).header('Authorization', this.token).body(params);
+		for (const [k, v] of Object.entries(query)) init.query(k, v);
+		const res = await init.send().catch(e => {
+			throw new Error(e);
+		});
 
-    return res.json();
-  }
+		return res.json();
+	}
 }
--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,113 +1,113 @@
 import { Snowflake } from 'discord-api-types';
 
 export interface ClientOptions {
-  /**
-   * Whether or not to include the full HTTP response from the API
-   */
-  readonly fullResponse: boolean
+	/**
+	 * Whether or not to include the full HTTP response from the API
+	 */
+	readonly fullResponse: boolean
 }
 
 type BaseResponse = {
-  /**
-   * Whether or not the request was a success
-   */
-  readonly error: boolean,
+	/**
+	 * Whether or not the request was a success
+	 */
+	readonly error: boolean,
 
-  /**
-   * An understandable response message
-   */
-  readonly message: string,
+	/**
+	 * An understandable response message
+	 */
+	readonly message: string,
 
-  /**
-   * The data being fetched
-   */
-  readonly data: CutterData | FarmData | PlotData | TreeData | FarmStorageData | UserData | VehicleData
+	/**
+	 * The data being fetched
+	 */
+	readonly data: CutterData | FarmData | PlotData | TreeData | FarmStorageData | UserData | VehicleData
 }
 
 export interface CutterResponse extends BaseResponse {
-  readonly data: CutterData
+	readonly data: CutterData
 }
 
 export interface FarmResponse extends BaseResponse {
-  readonly data: FarmData
+	readonly data: FarmData
 }
 
 export interface PlotResponse extends BaseResponse {
-  readonly data: PlotData
+	readonly data: PlotData
 }
 
 export interface TreeResponse extends BaseResponse {
-  readonly data: TreeData
+	readonly data: TreeData
 }
 
 export interface StorageResponse extends BaseResponse {
-  readonly data: FarmStorageData
+	readonly data: FarmStorageData
 }
 
 export interface VehicleResponse extends BaseResponse {
-  readonly data: VehicleData
+	readonly data: VehicleData
 }
 
 export interface UserResponse extends BaseResponse {
-  readonly data: UserData
+	readonly data: UserData
 }
 
 export type CutterData = {
-  readonly cooldown: number,
-  readonly cost: number,
-  readonly efficiency: number[],
-  readonly level: number,
-  readonly name: string
+	readonly cooldown: number,
+	readonly cost: number,
+	readonly efficiency: number[],
+	readonly level: number,
+	readonly name: string
 }
 
 export type FarmData = {
-  readonly name: string
-  readonly saplings: number
-  readonly tree: TreeData
-  readonly cutters: number
-  readonly plots: number
-  readonly storages: number
-  readonly vehicles: number
+	readonly name: string
+	readonly saplings: number
+	readonly tree: TreeData
+	readonly cutters: number
+	readonly plots: number
+	readonly storages: number
+	readonly vehicles: number
 }
 
 export type PlotData = {
-  readonly cost: number,
-  readonly level: number,
-  readonly growthSpeed: number
+	readonly cost: number,
+	readonly level: number,
+	readonly growthSpeed: number
 }
 
 export type FarmStorageData = {
-  readonly capacity: number
-  readonly cost: number
-  readonly level: number
-  readonly name: string
+	readonly capacity: number
+	readonly cost: number
+	readonly level: number
+	readonly name: string
 }
 
 export type UserData = {
-  readonly id: Snowflake
-  readonly createdAt: number
-  readonly banned: boolean
-  readonly goldenTrees: number
-  readonly language: string
-  readonly money: number
+	readonly id: Snowflake
+	readonly createdAt: number
+	readonly banned: boolean
+	readonly goldenTrees: number
+	readonly language: string
+	readonly money: number
 }
 
 export type VehicleData = {
-  readonly capacity: number
-  readonly cooldown: number
-  readonly cost: number
-  readonly level: number
-  readonly name: string
+	readonly capacity: number
+	readonly cooldown: number
+	readonly cost: number
+	readonly level: number
+	readonly name: string
 }
 
 export type TreeData = {
-  readonly level: number;
-  readonly cost: number;
-  readonly name: string;
-  readonly funFact: string;
-  readonly plantingRegion: string;
-  readonly growthTime: number;
-  readonly logCount: number;
-  readonly logValue: number;
-  readonly saplingCost: number;
+	readonly level: number;
+	readonly cost: number;
+	readonly name: string;
+	readonly funFact: string;
+	readonly plantingRegion: string;
+	readonly growthTime: number;
+	readonly logCount: number;
+	readonly logValue: number;
+	readonly saplingCost: number;
 }
